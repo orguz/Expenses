111e53492fd18131e042bf1fcf40cc6f8909ffb3
Or Guz <orguz100@gmail.com>
2015-02-28 16:45:40 +0200

    enable cors express1

--- app/config/express.js
+++ app/config/express.js
@@ -15,7 +15,7 @@ var config = require('config');
 
 module.exports = function (app) {
     var allowCrossDomain = function(req, res, next) {
-        res.header('Access-Control-Allow-Origin', 'calc-expenses.herokuapp.com');
+        res.header('Access-Control-Allow-Origin', 'http://calc-expenses.herokuapp.com');
         res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
         res.header('Access-Control-Allow-Headers', 'Content-Type');
 
